8efbe8536b5328fc8729116b45a4e217ddf56f4b
Lucy Shen <lucyshen77@gmail.com>
2021-12-16 05:22:27 +0000

    implement form validation and refetch tweets on submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,45 +5,30 @@
  */
 
 $(document).ready(function() {
-  // Fake data taken from initial-tweets.json
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
 
   const renderTweets = function(tweets) {
-    // loops through tweets
-    // calls createTweetElement for each tweet
-    // takes return value and appends it to the tweets container
-
+    $('#tweets-container').empty(); // empty container
+    const sortedTweets = tweets.sort((a, b) => { // sort tweets by date
+      if (a.created_at > b.created_at) {
+        return -1;
+      } else {
+        return 1;
+      }
+    })
     for (const tweet of tweets) {
       const $tweet = createTweetElement(tweet);
       $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
     }
-  }  
+  };  
 
   const createTweetElement = (tweet) => {
     const time = timeago.format(tweet.created_at);
+    // prevent XSS with escaping
+    const escape = function (str) { 
+      let div = document.createElement("div");
+      div.appendChild(document.createTextNode(str));
+      return div.innerHTML;
+    };
     let $tweet = `
     <article class="article">
       <header class="tweet-header">
@@ -61,7 +46,7 @@ $(document).ready(function() {
       </header>
 
       <div class="tweet-body">
-        <p>${tweet.content.text}</p>
+        <p>${escape(tweet.content.text)}</p>
       </div>
 
       <footer>
@@ -75,24 +60,48 @@ $(document).ready(function() {
     return $tweet;
   }
 
-  renderTweets(data);
-
-  // form submission with jQuery
+  // Form Submission using jQuery
   
   $('form').submit(function(event) { // event listener for submit event on the form element
     event.preventDefault();
-    const newTweet = $(this).serialize(); 
-    
-    $.ajax({
-      url: "http://localhost:8080/tweets/",
-      method: 'POST',
-      data: newTweet,
-      success: function (data) {
-        console.log('success');
-        console.log(this.data)
-      }
-    })
-    
+
+    const input = $(this).children('#tweet-text').val();
+    const tweetCount = $(this.counter).val();
+
+    if (input === "") {
+      alert("Tweet cannot be empty.");
+    } else if (tweetCount < 0) {
+      alert("Exceeded maximum characters.");
+    } else {
+      const newTweet = $(this).serialize(); 
+      $.ajax({
+        url: "/tweets/",
+        method: 'POST',
+        data: newTweet,
+        success: function() {
+          $('#tweet-text').val(''); // clear form after submission
+          loadTweets();
+        }
+      })
+    }
+
   });
+
+  // Fetch tweets with Ajax
+
+  const loadTweets = () => {
+    $.ajax({
+      url: "/tweets/",
+      method: 'GET'
+    })
+    .then((response) => {
+      renderTweets(response);
+    })
+    .catch((error) => {
+      console.log('error:', error);
+    });
+  }
+
+  loadTweets();
 
 })
